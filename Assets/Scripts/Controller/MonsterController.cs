fdd2566830c5895455abce4852a2a565f5ee90b7
HanSanghyun <hshf123@naver.com>
2022-10-19 23:57:40 +0900

    Monster 공격애니메이션

--- Assets/Scripts/Controller/MonsterController.cs
+++ Assets/Scripts/Controller/MonsterController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MonsterController : MonoBehaviour
 {
-
     RectTransform _transform;
     public Vector2 Position
     {
@@ -20,16 +19,61 @@ public class MonsterController : MonoBehaviour
     public float _speed = 200f;
     Animator _animator;
 
+    protected enum MonsterState
+    {
+        Move,
+        Attack,
+    }
+    MonsterState _state;
+    protected MonsterState State
+    {
+        get { return _state; }
+        set
+        {
+            if (_state == value)
+                return;
+
+            switch (value)
+            {
+                case MonsterState.Move:
+                    _animator.Play("Monster_Walk");
+                    break;
+                case MonsterState.Attack:
+                    _animator.Play("Monster_Attack");
+                    break;
+            }
+        }
+    }
+
     void Start()
     {
         _transform = GetComponent<RectTransform>();
         _animator = GetComponent<Animator>();
         _animator.Play("Monster_Walk");
         Position = new Vector2(0, 2000);
+        State = MonsterState.Move;
     }
 
     void Update()
     {
-        Position += new Vector2(0, -1f) * Time.deltaTime * _speed;
+        UpdateAnim();
+    }
+
+    void UpdateAnim()
+    {
+        if (Position.y <= -426f)
+        {
+            // TODO : 공격
+            State = MonsterState.Attack;
+            Position = new Vector2(0, -426f);
+
+        }
+        else
+        {
+            // TODO : 이동
+            State = MonsterState.Move;
+            Position += new Vector2(0, -1f) * Time.deltaTime * _speed;
+
+        }
     }
 }
